49ebdfc94ba7e400c448c056bc6ba05e6839f7b6
ay4shi <pedroverlord@gmail.com>
2025-11-25 16:38:43 -0300

    Script
    Captura da posição atual do cursor do mouse na tela.

--- script.py
+++ script.py
@@ -1,5 +1,3 @@
-# Posição do mouse
-
-import pyautogui as py
-py.sleep(5) # Tempo de espera do click
-print(py.position())
+import pyautogui as py # Importa a biblioteca pyautogui
+py.sleep(5) # Delay de 5 segundos
+print(py.position()) # Imprime a posição atual do cursor do mouse
